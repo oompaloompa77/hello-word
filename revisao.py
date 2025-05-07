d90b17409cc3fa2dc0526e80f79d74031f9ce7c8
oompaloompa77 <paula.mariana2019.pmom@gmail.com>
2025-05-07 15:26:29 -0300

    criou revisão

---
+++ revisao.py
@@ -0,0 +1,18 @@
+#comentário
+
+#variável é um espaço na memória que receberá dado
+#tipo  texto, número (int e flot(moeda, altura, peso
+
+#exemplo de números float (flutuante)
+peso = 89.5
+print(peso)
+
+#exemplo de números inteiros
+idade = 18
+print(idade)
+
+#exemplo de variáveis do tipo texto
+nome = ("Maria")
+sobrenome = "Silva"
+print(nome, sobrenome)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
